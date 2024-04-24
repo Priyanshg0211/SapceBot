1aded14562cc4f042991de62215f47bb6eb94aee
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-04-24 19:02:50 +0530

    created  homepage

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:spacebot/pages/home_page.dart';
+void main()=> runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.grey.shade900,
+        primaryColor: Colors.deepPurple.shade300,
+      ),
+    );
+  }
+}
