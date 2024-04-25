e58113ca63d318e183ca700928cf91308ad306fb
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-04-25 19:53:36 +0530

    Refactored ChatBloc constructor and added ChatSuccessState to initialize the messages list

--- lib/bloc/chat_bloc.dart
+++ lib/bloc/chat_bloc.dart
@@ -10,7 +10,7 @@ part 'chat_event.dart';
 part 'chat_state.dart';
 
 class ChatBloc extends Bloc<ChatEvent, ChatState> {
-  ChatBloc() : super(ChatInitial()) {
+  ChatBloc() : super(ChatSuccessState(messages: [])) {
     on<ChatGenerateNewTextMessageEvent>(chatGenerateNewTextMessageEvent);
   }
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spacebot/bloc/chat_bloc.dart';
+import 'package:spacebot/models/chat_message_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,68 +11,100 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final ChatBloc chatBloc = ChatBloc();
+  TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              height: 100,
-              child: const Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Space Pod",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                  Icon(Icons.image_search,color: Colors.white,)
-                ],
-              ),
-            ),
-            Expanded(child: ListView()),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 30, horizontal: 16),
-              child: Row(
-                children: [
-                  Expanded(
-                      child: TextField(
-                    style: TextStyle(color: Colors.black),
-                    cursorColor: Theme.of(context).primaryColor,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      fillColor: Colors.white,
-                      filled: true,
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                        borderSide:
-                            BorderSide(color: Theme.of(context).primaryColor),
+      body: BlocConsumer<ChatBloc, ChatState>(
+        bloc: chatBloc,
+        listener: (context, state) {},
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case ChatSuccessState:
+              List<ChatMessageModel> message =
+                  (state as ChatSuccessState).messages;
+              return Container(
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      height: 100,
+                      child: const Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Space Pod",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                          Icon(
+                            Icons.image_search,
+                            color: Colors.white,
+                          )
+                        ],
                       ),
                     ),
-                  )),
-                  const SizedBox(
-                    width: 12,
-                  ),
-                  CircleAvatar(
-                    radius: 32,
-                    backgroundColor: Colors.white,
-                    child: CircleAvatar(
-                      radius: 30,
-                      backgroundColor: Theme.of(context).primaryColor,
-                      child: Center(
-                        child: Icon(
-                          Icons.send,
-                          color: Colors.white,
-                        ),
+                    Expanded(child: ListView()),
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 30, horizontal: 16),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: TextField(
+                            style: TextStyle(color: Colors.black),
+                            cursorColor: Theme.of(context).primaryColor,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              fillColor: Colors.white,
+                              filled: true,
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50),
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor),
+                              ),
+                            ),
+                          )),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          InkWell(
+                            onTap: () {
+                              if (textEditingController.text.isNotEmpty) {
+                                chatBloc.add(ChatGenerateNewTextMessageEvent(
+                                    inputMessage: textEditingController.text));
+                              }
+                            },
+                            child: CircleAvatar(
+                              radius: 32,
+                              backgroundColor: Colors.white,
+                              child: CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Theme.of(context).primaryColor,
+                                child: Center(
+                                  child: Icon(
+                                    Icons.send,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+                  ],
+                ),
+              );
+
+            default:
+              return SizedBox();
+          }
+        },
       ),
     );
   }
