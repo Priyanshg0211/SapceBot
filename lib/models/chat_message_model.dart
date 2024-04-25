5f7e0beecff7b1822f4a7de502631f04522766fd
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-04-25 19:12:10 +0530

    generated JSON serialization

--- lib/models/chat_message_model.dart
+++ lib/models/chat_message_model.dart
@@ -1,13 +1,53 @@
-class ChatMessageModel {
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
 
+class ChatMessageModel {
   final String role;
   final List<ChatPartModel> parts;
+  ChatMessageModel({
+    required this.role,
+    required this.parts,
+  });
 
-  ChatMessageModel(this.role, this.parts);
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'role': role,
+      'parts': parts.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory ChatMessageModel.fromMap(Map<String, dynamic> map) {
+    return ChatMessageModel(
+      role: map['role'] as String,
+      parts: List<ChatPartModel>.from((map['parts'] as List<int>).map<ChatPartModel>((x) => ChatPartModel.fromMap(x as Map<String,dynamic>),),),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ChatMessageModel.fromJson(String source) => ChatMessageModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
-class ChatPartModel{
+
+class ChatPartModel {
   final String text;
   ChatPartModel({
     required this.text,
   });
+
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'text': text,
+    };
+  }
+
+  factory ChatPartModel.fromMap(Map<String, dynamic> map) {
+    return ChatPartModel(
+      text: map['text'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ChatPartModel.fromJson(String source) => ChatPartModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
