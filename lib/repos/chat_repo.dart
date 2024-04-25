dd99ce44a14ae49d71bebd43aa936ae72a3e8627
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-04-25 19:21:07 +0530

    call the api in chat_repo.dart file

--- lib/repos/chat_repo.dart
+++ lib/repos/chat_repo.dart
@@ -1,15 +1,47 @@
+import 'dart:math';
+
 import 'package:dio/dio.dart';
 import 'package:spacebot/models/chat_message_model.dart';
 import 'package:spacebot/utils/constants.dart';
 
 class SpaceRepo {
-  static chatTextGenerationRepo(
-      List<ChatMessageModel> previousMessages) async {
-        Dio dio = Dio();
-        
+  static chatTextGenerationRepo(List<ChatMessageModel> previousMessages) async {
+    try {
+      Dio dio = Dio();
 
-
-        final response = dio.post("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-latest:generateContent?key=${apikey}",
-        );
-      }
+      final response = dio.post(
+          "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-latest:generateContent?key=${apikey}",
+          data: {
+            "contents": previousMessages.map((e) => e.toMap()).toList(),
+            "generationConfig": {
+              "temperature": 1,
+              "topK": 0,
+              "topP": 0.95,
+              "maxOutputTokens": 8192,
+              "stopSequences": []
+            },
+            "safetySettings": [
+              {
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+              },
+              {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+              },
+              {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+              },
+              {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+              }
+            ]
+          });
+          log(response.toString() as num);
+    } catch (e) {
+      log(e.toString() as num);
+    }
+  }
 }
